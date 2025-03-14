3c09e6f584153a0cbf27c4cc98e7f70925735ab6
MOHD SALMAN <salman1256@yahoo.com>
2025-03-14 14:57:03 +0530

    Some Changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*** DAY2 APP ***");
+Console.WriteLine("*** Welcome to Employee Management App ***");
 List<Employee> lstEmployees=new List<Employee> 
 {
     new Employee{Id=1,Name="Sam",Designation="Manager",Salary=98000.70,
@@ -7,15 +7,8 @@ List<Employee> lstEmployees=new List<Employee>
     new Employee{Id=2,Name="Raj",Designation="Developer",Salary=58000.70,
     DoJ=new DateTime(day:12,month:03,year:2022)},
     new Employee{Id=3,Name="Rinu",Designation="Hr",Salary=45000.50,
-    DoJ=new DateTime(day:21,month:02,year:2022)},
-    new Employee{Id=4,Name="Suhail",Designation="Developer",Salary=98000.20,
-    DoJ=new DateTime(day:12,month:12,year:2020)},
-    new Employee{Id=5,Name="Deep",Designation="Tester",Salary=7500.00,
-    DoJ=new DateTime(day:09,month:09,year:2023)},
-    new Employee{Id=09,Name="Rohit",Designation="Developerr",Salary=75000.00,
-    DoJ=new DateTime(day:09,month:09,year:2023)  },
-    new Employee{Id=10,Name="Nitin",Designation="Developer",Salary=75000.00,
-    DoJ=new DateTime(day:09,month:09,year:2023),   }
+    DoJ=new DateTime(day:21,month:02,year:2022)}
+       
 
 };
 Console.WriteLine($"Number of Employees:{lstEmployees.Count}");
